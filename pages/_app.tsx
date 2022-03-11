8c6a168d363d4cc884ab3d555b4327f3c49d2716
adarsh500 <adarshsulegai50@gmail.com>
2022-03-12 00:40:40 +0530

    design: revamped UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,26 +4,35 @@ import Link from 'next/link'
 
 function MyApp({ Component, pageProps }: AppProps) {
    return (
-     <div>
-       <ul>
-         <li>
-           <Link href="/news">
-             <a>News</a>
+     <>
+       <header>
+         <div className="container">
+           <Link href="/">
+             <h2 className="title">Rapidashboard</h2>
            </Link>
-         </li>
-         <li>
-           <Link href="/users">
-             <a>Users</a>
-           </Link>
-         </li>
-         <li>
-           <Link href="/top">
-             <a>Top Users</a>
-           </Link>
-         </li>
-       </ul>
+           <nav>
+             <ul>
+               <li>
+                 <Link href="/news">
+                   <a>News</a>
+                 </Link>
+               </li>
+               <li>
+                 <Link href="/users">
+                   <a>Users</a>
+                 </Link>
+               </li>
+               <li>
+                 <Link href="/top">
+                   <a>Top Users</a>
+                 </Link>
+               </li>
+             </ul>
+           </nav>
+         </div>
+       </header>
        <Component {...pageProps} />
-     </div>
+     </>
    );
 }
 
